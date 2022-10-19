479befb51e3d1757f1d4940329c7f3f02048bd39
Jonathan Valenzuela Trujillo <jonathanvalenzuelatrujillo@gmail.com>
2022-10-19 02:41:32 -0500

    secommit

--- app/src/main/java/com/example/restaurant_app/MainActivity.kt
+++ app/src/main/java/com/example/restaurant_app/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.restaurant_app
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun ingresar(View:View)
+    {
+            val i = Intent(this, LoginActivity::class.java)
+            startActivity(i)
+            Toast.makeText(this, "Inicia sesion o Registrate", Toast.LENGTH_LONG).show()
+    }
+
 }
