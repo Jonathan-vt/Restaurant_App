aabb5b026096ce4b518aa92ce062c0e8b648949a
Jonathan Valenzuela Trujillo <jonathanvalenzuelatrujillo@gmail.com>
2022-10-20 19:50:26 -0500

    version funcinal incompleta copia1

--- app/src/main/java/com/example/restaurant_app/RegisterActivity.kt
+++ app/src/main/java/com/example/restaurant_app/RegisterActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
